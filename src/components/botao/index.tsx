b746e78f9a78c46f3891e9bb63777ce097783249
andresinho20049 <andre.andresinho2009@hotmail.com>
2024-05-11 23:57:58 -0300

    set type Button to receive ButtonTypes HTMLButtonElement

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,15 +1,14 @@
-import { Component, ReactNode } from "react";
+import { ButtonHTMLAttributes, Component, ReactNode } from "react";
 import styles from "./Botao.module.scss";
 
-type BotaoPropsType = {
-  type?: "submit" | "reset" | "button";
+type BotaoPropsType = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
 };
 
 class Botao extends Component<BotaoPropsType> {
   render() {
     return (
-      <button type={this.props.type} className={styles.botao}>
+      <button {...this.props} className={styles.botao}>
         {this.props.children}
       </button>
     );
